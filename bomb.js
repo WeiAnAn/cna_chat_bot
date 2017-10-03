171435b88cd618c6318722228b4496e51e6e7492
WeiAnAn <j22461615@gmail.com>
2017-10-03 15:52:08 +0800

    add error hadling

--- bomb.js
+++ bomb.js
@@ -1,9 +1,14 @@
-const sendTextMessage = require("./send.js");
+const sendTextMessage = require("./send.js").sendTextMessage;
 const db = require("./db.js");
 
 db.query("SELECT `msg_id` FROM `users` WHERE msg_id IS NOT NULL", function(err, results){
     db.end();
     results.forEach((user)=>{
-        Promise.resolve().then(()=>{sendTextMessage(user.msg_id, "小八小藍機器爆炸拉!")});
+        Promise.resolve()
+            .then(()=>{
+                sendTextMessage(user.msg_id, "小八小藍機器爆炸拉!")}
+            ).catch((err)=>{
+                console.log(err)
+            });
     });
 });
