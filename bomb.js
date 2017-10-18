764e70c576a1405cc2fc577fb71da798c7cc00d6
unknown <>
2017-10-18 23:00:36 +0800

    end db pool

--- bomb.js
+++ bomb.js
@@ -26,12 +26,14 @@ connection.query(sql, function(err, rows){
             let message = "[爆炸] 一堆機器 下線啦!! 詳見S_monitor";
             
             db.execute("SELECT `msg_id` FROM `Admin` WHERE msg_id IS NOT NULL AND `graduated` = 0 AND gid <= 8 AND enable = 1", function(err, results){
+                db.end();
                 results.forEach((users)=>{
                     sendTextMessage(users.msg_id, message);
                 });
             });
         }else{
             db.execute("SELECT `msg_id` FROM `Admin` WHERE msg_id IS NOT NULL AND `graduated` = 0 AND gid <= 8 AND enable = 1", function(err, results){
+                db.end();
                 rows.forEach((machine)=>{
                     let message = `${machine.description} ( ${machine.hostname} ) 下線啦!!`;
                     results.forEach((users)=>{
