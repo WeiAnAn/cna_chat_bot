3b252951ac5aa3b0e97487ad81279dc1dd16b6c8
unknown <>
2017-10-18 22:56:52 +0800

    wrong table name

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const db = require("./src/utils/db");
 
 let message = "[爆炸] 一堆機器 下線啦!! 詳見S_monitor";
 
-db.execute("SELECT `msg_id` FROM `admin` WHERE msg_id IS NOT NULL AND `graduated` = 0 AND gid <= 8 AND enable = 1", function(err, results){
+db.execute("SELECT `msg_id` FROM `Admin` WHERE msg_id IS NOT NULL AND `graduated` = 0 AND gid <= 8 AND enable = 1", function(err, results){
     db.end();
     results.forEach((users)=>{
         sendTextMessage(users.msg_id, message);
