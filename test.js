84587e75c08b31a4b61b9683b3be914e2173f4e7
unknown <>
2017-10-18 22:28:12 +0800

    add test.js for test

---
+++ test.js
@@ -0,0 +1,10 @@
+const sendTextMessage = require("./src/utils/send.js").sendTextMessage;
+const db = require("./src/utils/db");
+
+let message = "[爆炸] 一堆機器 下線啦!! 詳見S_monitor";
+
+db.execute("SELECT `msg_id` FROM `admin` WHERE msg_id IS NOT NULL AND `graduated` IS 0 AND gid <= 8 AND enable IS 1", function(err, results){
+    results.foreach((users)=>{
+        sendTextMessage(users.msg_id, message);
+    });
+});
