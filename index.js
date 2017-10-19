9419fd8d69df2ff524e97cbffefc24f9cc74e876
WeiAnAn <j22461615@gmail.com>
2017-10-19 16:05:25 +0800

    add helmet

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const router = require("./src/routers/router.js");
+const helmet = require("helmet");
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(router);    
